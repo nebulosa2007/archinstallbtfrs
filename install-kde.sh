02c8e3c418026c0514b73b2fe4359021e4676e2e
nebulosa2007 <85841412+nebulosa2007@users.noreply.github.com>
2023-01-12 02:20:34 +0300

    Update install-kde.sh

--- install-kde.sh
+++ install-kde.sh
@@ -1,3 +1,18 @@
 ## INSTALLING KDE
 
-...
+# Step 1. Install Xorg
+sudo pacman -S xorg-server
+
+# Install drivers 
+# lspci -v | grep -A1 -e VGA -e 3D
+# https://wiki.archlinux.org/title/Xorg#Driver_installation
+# or all of them:
+# sudo pacman -S xorg-drivers
+
+# Testing X.Org Server
+# sudo pacman -S xorg-xinit xterm
+# startx
+
+# Step 2. Install plasma, sddm
+sudo pacman -S plasma-desktop sddm
+sudo systemctl enable --now sddm
