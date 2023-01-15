31736b39d64b9e4bbc1dd598e4a233d5c059544d
nebulosa2007 <85841412+nebulosa2007@users.noreply.github.com>
2023-01-16 00:24:38 +0300

    Update install-kde.sh

--- install-kde.sh
+++ install-kde.sh
@@ -6,6 +6,8 @@ sudo pacman -S xorg-server
 # Install drivers 
 # lspci -v | grep -A1 -e VGA -e 3D
 # https://wiki.archlinux.org/title/Xorg#Driver_installation
+# ATI + Intel
+# sudo pacman -S xf86-video-ati mesa xf86-video-intel libva-intel-driver  libva-mesa-driver libva-vdpau-driver
 # or all of them:
 # sudo pacman -S xorg-drivers
 
@@ -16,19 +18,19 @@ sudo pacman -S xorg-server
 # Step 2.
 
 ## KDE Desktop minimal
-sudo pacman -S plasma-desktop yakuake dolphin spectacle kate sddm
-sudo pacman -S --asdeps qt5-xmlpatterns plasma-browser-integration plasma-pa sddm-kcm
+sudo pacman -S plasma-desktop qt5-xmlpatterns plasma-browser-integration plasma-pa power-profiles-daemon kdeplasma-addons sddm sddm-kcm dolphin yakuake spectacle kate
 # Optinal: time without PM/AM
 # echo "LANG=en_US.UTF-8" | sudo tee /etc/default/locale
 # sudo mkdir /etc/systemd/system/sddm.service.d/ && printf "[Service]\nEnvironmentFile=-/etc/default/locale" | sudo tee /etc/systemd/system/sddm.service.d/10-time.conf && sudo systemctl daemon-reload
-sudo systemctl enable --now sddm
 
 # Without networkmanager and bluez
-pikaur -S --asdeps powerdevil-light
+pikaur -S powerdevil-light
 
 #Bluetooth support
-sudo pacman -S --asdeps pulseaudio-bluetooth bluedevil
+sudo pacman -S pulseaudio-bluetooth bluedevil
 sudo systemctl enable --now bluetooth
 
 #Android integration
-sudo pacman -S --asdeps kdeconnect sshfs
+sudo pacman -S kdeconnect sshfs
+
+sudo systemctl enable --now sddm
