273988adb1a3fc6589c60951163fdd42fe6a8d99
nebulosa2007 <85841412+nebulosa2007@users.noreply.github.com>
2023-01-26 11:50:11 +0300

    Update !install_system_btfrs.sh

--- !install_system_btfrs.sh
+++ !install_system_btfrs.sh
@@ -134,6 +134,8 @@ echo "zram" > /etc/modules-load.d/zram.conf
 echo "options zram num_devices=1" > /etc/modprobe.d/zram.conf
 echo 'KERNEL=="zram0", ATTR{disksize}="'$(awk '/MemTotal/ {print $2}' /proc/meminfo)'K" RUN="/usr/bin/mkswap /dev/zram0", TAG+="systemd"' > /etc/udev/rules.d/99-zram.rules
 echo "/dev/zram0     none     swap     defaults     0     0" >> /etc/fstab
+#Optional:
+#systemctl mask udisks2-zram-setup@zram0.service
 
 #Other
 systemctl enable reflector.timer
