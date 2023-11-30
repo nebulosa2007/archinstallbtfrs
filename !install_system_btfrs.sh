f9edc4a636b2b14ff18d1ba06393b79a6c84113d
nebulosa2007 <85841412+nebulosa2007@users.noreply.github.com>
2023-11-30 08:50:28 +0300

    Update !install_system_btfrs.sh

--- !install_system_btfrs.sh
+++ !install_system_btfrs.sh
@@ -160,7 +160,7 @@ grub-install --target=i386-pc --recheck /dev/sda #Or /dev/vda for VPS
 # sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 ## https://ventureo.codeberg.page/source/kernel-parameters.html
 # sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash rootfstype=btrfs raid=noautodetect mitigations=off preempt=none audit=0 page_alloc.shuffle=1 split_lock_detect=off pci=pcie_bus_perf"/' /etc/default/grub
-# sed -i "s/rootfstype=btrfs /rootfstype=btrfs lpj=$(dmesg | grep -Po '(?<=lpj=)(\d+)') /" /etc/default/grub
+# sed -i "s/rootfstype=btrfs /rootfstype=btrfs lpj=$(dmesg | grep -Po '(?<=BogoMIPS\(lpj=)(\d+)') /" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #Double check files!
