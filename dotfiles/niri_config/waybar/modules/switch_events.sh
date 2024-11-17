77e07494a35f27c4b8319e13d70b113955356716
Nebulosa <nebulosa2007@yandex.ru>
2024-11-17 13:47:31 +0300

    Update configs for Niri >=0.1.10.1 and Waybar >=0.11.0

---
+++ dotfiles/niri_config/waybar/modules/switch_events.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+case "$1" in
+btreload)
+    # Fix bluetooth repairing loudspeaker
+    if bluetoothctl show | grep 'Powered: yes' -q; then
+        bluetoothctl power off &> /dev/null && \
+        sleep 1 && \
+        bluetoothctl power on &> /dev/null
+    fi
+esac
+
