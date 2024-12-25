e3512a38a40b544295a2ce98c412dc9d08844c35
Nebulosa <85841412+nebulosa2007@users.noreply.github.com>
2024-12-25 10:31:27 +0300

    Update install_gnome.sh

--- install_gnome.sh
+++ install_gnome.sh
@@ -3,12 +3,6 @@
 
 ## INSTALLING GNOME AND TIMESHIFT
 
-#pikaur - AUR helper, smallest one
-sudo pacman -S --needed git
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur && makepkg -fsri && cd .. && rm -rf pikaur
-
-
 #Installing Gnome
 sudo pacman -S gdm gnome gnome-tweaks gnome-software-packagekit-plugin gnome-extra xorg arc-icon-theme arc-gtk-theme xdg-user-dirs
 sudo systemctl enable --now gdm
@@ -18,12 +12,12 @@ sudo systemctl enable --now gdm
 #git clone https://aur.archlinux.org/timeshift.git
 #cd timeshift && makepkg -fsri && cd .. && rm -fr timeshift
 #or
-pikaur -S timeshift
+paru -S timeshift
 
 #git clone https://aur.archlinux.org/timeshift-autosnap.git
 #cd timeshift-autosnap && makepkg -fsri && cd .. && rm -fr timeshift-autosnap
 #or
-pikaur -S timeshift-autosnap
+paru -S timeshift-autosnap
 #Check everything and create first snapshot
 sudo timeshift-autosnap
 
